38082e087b5794697d7fc6f668acabe6dd40baa1
Dhaksana <113767440+Dhaksana@users.noreply.github.com>
2026-02-10 17:37:32 +0530

    Basic Authentication done

---
+++ Backend/src/main/java/com/bd/config/UserConfig.java
@@ -0,0 +1,25 @@
+package com.bd.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+
+        UserDetails user = User.builder()
+                .username("user1")
+                .password(encoder.encode("password123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
